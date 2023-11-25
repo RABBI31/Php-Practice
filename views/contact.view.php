3357386e06cfb6366dce26fea99e91125f705943
Md.Ali Rabbi <56753742+RABBI31@users.noreply.github.com>
2023-11-26 01:36:46 +0600

    Day-1

---
+++ views/contact.view.php
@@ -0,0 +1,12 @@
+<?php require('partials/Head.php') ?>>
+<?php require('partials/Navber.php') ?>
+<?php require('partials/Header.php') ?>
+  <main class="min-h-full">
+    <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+      <h1 class="text-3xl">Hello Good People, Welcome to the Contact</h1>
+    </div>
+  </main>
+</div>
+
+</body>
+</html>
